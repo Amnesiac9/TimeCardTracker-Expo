ddb720997c01a0ea1be6dfd2060ceda6c694993a
John <johnmoreau19@gmail.com>
2024-02-03 19:10:59 -0800

    Moved files around to adjust structure of tabs

--- app/(drawer)/_layout.tsx
+++ app/(drawer)/_layout.tsx
@@ -4,46 +4,48 @@ import { Drawer } from 'expo-router/drawer';
 import { Pressable, StyleSheet } from 'react-native';
 
 const DrawerLayout = () => (
-  <Drawer>
-    <Drawer.Screen
-      name="index"
-      options={{
-        headerTitle: 'Home',
-        drawerLabel: 'Home',
-        drawerIcon: ({ size, color }) => <Ionicons name="home-outline" size={size} color={color} />,
-      }}
-    />
-    <Drawer.Screen
-      name="(tabs)"
-      options={{
-        headerTitle: 'Tabs',
-        drawerLabel: 'Tabs',
-        drawerIcon: ({ size, color }) => (
-          <MaterialIcons name="border-bottom" size={size} color={color} />
-        ),
-        headerRight: () => (
-          <Link href="/modal" asChild>
-            <Pressable>
-              {({ pressed }) => (
-                <FontAwesome
-                  name="info-circle"
-                  size={25}
-                  color="gray"
-                  style={[styles.headerRight, { opacity: pressed ? 0.5 : 1 }]}
-                />
-              )}
-            </Pressable>
-          </Link>
-        ),
-      }}
-    />
-  </Drawer>
+    <Drawer>
+        <Drawer.Screen
+            name="index"
+            options={{
+                headerTitle: 'Home',
+                drawerLabel: 'Home',
+                drawerIcon: ({ size, color }) => (
+                    <Ionicons name="home-outline" size={size} color={color} />
+                ),
+            }}
+        />
+        <Drawer.Screen
+            name="(tabs)"
+            options={{
+                headerTitle: 'Tabs',
+                drawerLabel: 'Tabs',
+                drawerIcon: ({ size, color }) => (
+                    <MaterialIcons name="border-bottom" size={size} color={color} />
+                ),
+                headerRight: () => (
+                    <Link href="/modal" asChild>
+                        <Pressable>
+                            {({ pressed }) => (
+                                <FontAwesome
+                                    name="info-circle"
+                                    size={25}
+                                    color="gray"
+                                    style={[styles.headerRight, { opacity: pressed ? 0.5 : 1 }]}
+                                />
+                            )}
+                        </Pressable>
+                    </Link>
+                ),
+            }}
+        />
+    </Drawer>
 );
 
 const styles = StyleSheet.create({
-  headerRight: {
-    marginRight: 15,
-  },
+    headerRight: {
+        marginRight: 15,
+    },
 });
 
 export default DrawerLayout;
