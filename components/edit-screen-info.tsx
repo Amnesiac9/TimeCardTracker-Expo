14e89236e178dcc50c312013972606c6fd268133
John <johnmoreau19@gmail.com>
2024-02-03 17:51:39 -0800

    Updated prettier rules and line spacing

--- components/edit-screen-info.tsx
+++ components/edit-screen-info.tsx
@@ -1,30 +1,22 @@
-
-    import { Text, View } from 'react-native';
-
+import { Text, View } from 'react-native';
 
 export default function EditScreenInfo({ path }: { path: string }) {
-  
-    
-        return (
-            <View>
-                <View className={styles.getStartedContainer}>
-                    <Text className={styles.getStartedText}>
-                        Open up the code for this screen:
-                    </Text>
-                    <View className={styles.codeHighlightContainer + styles.homeScreenFilename}>
-                        <Text>{path}</Text>
-                    </View>
-                    <Text className={styles.getStartedText}>
-                        Change any of the text, save the file, and your app will automatically update.
-                    </Text>
+    return (
+        <View>
+            <View className={styles.getStartedContainer}>
+                <Text className={styles.getStartedText}>Open up the code for this screen:</Text>
+                <View className={styles.codeHighlightContainer + styles.homeScreenFilename}>
+                    <Text>{path}</Text>
                 </View>
+                <Text className={styles.getStartedText}>
+                    Change any of the text, save the file, and your app will automatically update.
+                </Text>
             </View>
-        );
-    
+        </View>
+    );
 }
 
-
-    const styles = {
+const styles = {
     codeHighlightContainer: `rounded-md px-1`,
     getStartedContainer: `items-center mx-12`,
     getStartedText: `text-lg leading-6 text-center`,
@@ -32,4 +24,4 @@ export default function EditScreenInfo({ path }: { path: string }) {
     helpLink: `py-4`,
     helpLinkText: `text-center`,
     homeScreenFilename: `my-2`,
-    };
+};
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    
-      content: ["./index.{js,ts,tsx}", "./app/**/*.{js,ts,tsx}"],
-    
-    presets: [require("nativewind/preset")],
+    content: ['./index.{js,ts,tsx}', './app/**/*.{js,ts,tsx}'],
+    presets: [require('nativewind/preset')],
     theme: {
-      extend: {},
+        extend: {},
     },
     plugins: [],
-  }
+};
