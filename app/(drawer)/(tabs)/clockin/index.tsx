42f21db43dbad1ac01ee82042d83514ef3acfdc1
John <johnmoreau19@gmail.com>
2024-02-04 14:15:50 -0800

    Messing around with React Native Paper

--- app/(drawer)/(tabs)/clockin/index.tsx
+++ app/(drawer)/(tabs)/clockin/index.tsx
@@ -1,27 +1,32 @@
 import { useColorScheme } from 'nativewind';
-import { Button, Text, View } from 'react-native';
+import { View } from 'react-native';
+import { Button, Text } from 'react-native-paper';
 
 import EditScreenInfo from '~/components/edit-screen-info';
 
 const Page = () => {
-    const { colorScheme, setColorScheme } = useColorScheme();
-
     return (
         <View className={styles.container}>
-            <View>
+            <View className={styles.titleArea}>
                 <Text className={styles.title}>Clock In</Text>
                 <View className={styles.separator} />
             </View>
             <View className={styles.buttonArea}>
-                <Button title="Add Time" onPress={() => {}} />
+                <Button mode="contained" onPress={() => {}}>
+                    Add Time
+                </Button>
                 <View className={styles.separator} />
-                <Button color="red" title="Clock In" onPress={() => {}} />
+                <Button mode="contained" onPress={() => {}}>
+                    Clock In
+                </Button>
                 <View className={styles.separator} />
-                <Button title="View Shifts" onPress={() => {}} />
+                <Button mode="contained" onPress={() => {}}>
+                    View Shifts
+                </Button>
                 <View className={styles.blankSeperator} />
-                <Text onPress={() => setColorScheme(colorScheme === 'light' ? 'dark' : 'light')}>
+                {/* <Text onPress={() => setColorScheme(colorScheme === 'light' ? 'dark' : 'light')}>
                     {`The color scheme is ${colorScheme}`}
-                </Text>
+                </Text> */}
                 <EditScreenInfo path="app/(tabs)/clockin/index.tsx" />
             </View>
         </View>
@@ -31,11 +36,12 @@ const Page = () => {
 export default Page;
 
 const styles = {
-    container: `flex-1 bg-white dark:bg-black`, // Align items to the top and add padding
+    container: `flex-1 b`, // Align items to the top and add padding g-white dark:bg-black
     buttonArea: `items-center flex-1 justify-center`,
     center: `justify-center`, // Center the content
     separator: `text-center h-[1px] my-7 w-4/5 bg-gray-200`,
     blankSeperator: `h-5`, // Add a small amount of space
     title: `text-center text-4xl mt-5 pt-5`, // Increase text size
+    titleArea: `flex-1 items-center`,
     space: `my-12`,
 };
--- app/index.tsx
+++ app/index.tsx
@@ -1,18 +1,9 @@
 import { Redirect } from 'expo-router';
-import { StatusBar } from 'expo-status-bar';
-import { useColorScheme } from 'nativewind';
 import React from 'react';
-import { View, Text } from 'react-native';
 
 const MainScreen = () => {
-    const { colorScheme } = useColorScheme();
-    return (
-        <View className="flex flex-1 bg-white dark:bg-black">
-            <StatusBar />
-            {/* <Redirect href="/(drawer)/(tabs)/clockin" /> */}
-            <Text>Redirect to Clock In</Text>
-        </View>
-    );
+    // const { colorScheme } = useColorScheme();
+    return <Redirect href="/(drawer)/(tabs)/clockin" />;
 };
 
 export default MainScreen;
